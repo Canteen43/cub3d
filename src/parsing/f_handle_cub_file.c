bac35ef40b07212557c285d4c1c09945f62afb4d
Karl's WSL <karl.weihmann@gmail.com>
2025-01-22 10:09:20 +0100

    fix: segfault removed by moving image loading

--- src/parsing/f_handle_cub_file.c
+++ src/parsing/f_handle_cub_file.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   f_handle_cub_file.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kweihman <kweihman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kweihman <kweihman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 16:27:30 by kweihman          #+#    #+#             */
-/*   Updated: 2025/01/21 18:23:57 by kweihman         ###   ########.fr       */
+/*   Updated: 2025/01/22 09:30:12 by kweihman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,4 @@ void	f_handle_cub_file(t_game *game, char **argv)
 	f_import_cub_file(game, argv);
 	f_set_config_data(game);
 	f_set_map(game);
-	f_load_textures(game);
 }
--- src/rendering/f_start_mlx.c
+++ src/rendering/f_start_mlx.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   f_start_mlx.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kweihman <kweihman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kweihman <kweihman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 12:12:45 by glevin            #+#    #+#             */
-/*   Updated: 2025/01/21 14:48:56 by kweihman         ###   ########.fr       */
+/*   Updated: 2025/01/22 09:29:40 by kweihman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	f_start_mlx(t_game *game)
 {
 	game->mlx = mlx_init();
+	f_load_textures(game);
 	game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "cub3D");
 	game->img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
 	game->data = mlx_get_data_addr(game->img, &game->bpp, &game->size_line,
