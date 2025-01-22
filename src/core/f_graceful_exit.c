d5e8ad6930cff9af67828fdf2a7c046d3dada69b
Canteen43 <karl.weihmann@gmail.com>
2025-01-22 14:22:58 +0100

    feat: mlx_destroy on exit

--- src/core/f_graceful_exit.c
+++ src/core/f_graceful_exit.c
@@ -6,17 +6,37 @@
 /*   By: kweihman <kweihman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 17:41:41 by kweihman          #+#    #+#             */
-/*   Updated: 2025/01/21 14:07:43 by kweihman         ###   ########.fr       */
+/*   Updated: 2025/01/22 14:22:33 by kweihman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+// Static functions:
+static void	sf_cleanup_images(t_game *game);
+
 void	f_graceful_exit(t_game *game, int exit_code, const char *func,
 		char *message)
 {
+	sf_cleanup_images(game);
+	if (game->win)
+		mlx_destroy_window(game->mlx, game->win);
 	if (exit_code)
 		f_print_error(func, message);
 	f_gc_clean(game);
 	exit(exit_code);
+}
+
+static void	sf_cleanup_images(t_game *game)
+{
+	if (game->north.img_ptr)
+		mlx_destroy_image(game->mlx, game->north.img_ptr);
+	if (game->east.img_ptr)
+		mlx_destroy_image(game->mlx, game->east.img_ptr);
+	if (game->south.img_ptr)
+		mlx_destroy_image(game->mlx, game->south.img_ptr);
+	if (game->west.img_ptr)
+		mlx_destroy_image(game->mlx, game->west.img_ptr);
+	if (game->img)
+		mlx_destroy_image(game->mlx, game->img);
 }
