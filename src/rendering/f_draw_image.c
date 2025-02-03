75b3b28fadaff4ac1b49a01f15588831121f2643
Canteen43 <karl.weihmann@gmail.com>
2025-02-03 13:28:54 +0100

    I am commited
    fix: added guard so space only works in bonus

--- src/rendering/f_draw_image.c
+++ src/rendering/f_draw_image.c
@@ -6,7 +6,7 @@
 /*   By: kweihman <kweihman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 11:57:34 by kweihman          #+#    #+#             */
-/*   Updated: 2025/02/02 12:19:33 by kweihman         ###   ########.fr       */
+/*   Updated: 2025/02/02 15:13:05 by kweihman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ void	f_draw_image(t_game *game, t_tex *tex, t_int_xy top_left)
 		while (x < tex->width)
 		{
 			color = f_get_pixel(game, tex, (t_coords){x, y});
-			// printf("%d, %d, %X\n", x, y, color);
 			f_put_pixel(top_left.x + x, top_left.y + y, color, game);
 			x++;
 		}
--- src/rendering/f_key_press.c
+++ src/rendering/f_key_press.c
@@ -6,7 +6,7 @@
 /*   By: kweihman <kweihman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 12:15:12 by glevin            #+#    #+#             */
-/*   Updated: 2025/02/01 19:08:27 by kweihman         ###   ########.fr       */
+/*   Updated: 2025/02/03 13:27:54 by kweihman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	f_key_press(int keycode, t_game *game)
 		game->right_rotate = true;
 	if (keycode == XK_Return)
 		f_debug_info(game);
-	if (keycode == XK_space)
+	if (keycode == XK_space && game->bonus)
 		f_remove_obstacle(game);
 	return (0);
 }
